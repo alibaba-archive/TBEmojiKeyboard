2e74490bfdb50faa85b0c62069e69c3d0c293dd8
HarriesChen <info@mrchenhao.com>
2015-08-24 15:15:02 +0800

    Initial Commit

---
+++ TBEmojiKeyBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TBEmojiKeyBoard
+//
+//  Created by ChenHao on 8/24/15.
+//  Copyright (c) 2015 Teambition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
